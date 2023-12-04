681401ae07060c4e4c5adda7001b72a1165d3e18
kas <107382862+s1280061@users.noreply.github.com>
2023-12-05 03:23:43 +0900

    Add files via upload

--- MLP_pruned.py
+++ MLP_pruned.py
@@ -1,22 +1,20 @@
-#成功したもの
-from torch.utils.data import DataLoader
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
-from sklearn.model_selection import KFold
-import numpy as np
+from torch.utils.data import Dataset
 from torch.optim.lr_scheduler import CosineAnnealingLR
+import numpy as np
+from sklearn.model_selection import KFold
 import pandas as pd
 import torch.nn.utils.prune as prune
 import csv
 import time
-import matplotlib.pyplot as plt
-import torch.autograd as autograd
 import psutil
 import gc
-from torch.utils.benchmark import Timer
+import torch.profiler
 import torch.nn.functional as F
+from torch.utils.benchmark import Timer
 
 class NN(nn.Module):
     def __init__(self):
@@ -38,7 +36,8 @@ class NN(nn.Module):
         x = self.layer2(x)
         y = self.decoder(x)
         return y
-class MyDataset(torch.utils.data.Dataset):
+
+class MyDataset(Dataset):
     def __init__(self, X, y):
         self.X = torch.tensor(X, dtype=torch.float32)
         self.y = torch.tensor(y, dtype=torch.float32)
@@ -49,19 +48,14 @@ class MyDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.X)
 
-start_time = time.time()
-dataset = np.loadtxt("/Users/kas/Desktop/EDFA_ML-main/EDFA_1_18dB.csv", delimiter=",")
-X = dataset[:, 1:86]
-y = dataset[:, 86:]
-
 def get_memory_usage():
-    # プロセスのメモリ使用量を取得
     process = psutil.Process()
     memory_info = process.memory_info()
-    # メモリ使用量を表示
     print(f"Memory Usage: {memory_info.rss / (1024 * 1024):.2f} MB")
-def train(model, train_loader, optimizer):
+
+def train(model, train_loader, optimizer, scheduler):
     model.train()
+
     for data, target in train_loader:
         optimizer.zero_grad()
         output = model(data)
@@ -79,79 +73,117 @@ def evaluate(model, val_loader):
             val_loss += torch.sqrt(nn.MSELoss()(output, target) + 1e-6).item()
     return val_loss / len(val_loader)
 
-model = NN()
-
 def evaluate_bias(model, X, y):
     model.eval()
     with torch.no_grad():
         inputs = torch.tensor(X, dtype=torch.float32)
-        predictions = model(inputs).numpy()  # テンソルをNumPy配列に変換
-    errors = predictions - y
-    if len(errors) == 0:
+        predictions = model(inputs).numpy()
+    differences = predictions - y
+    if len(differences) == 0:
         return 0
     else:
-        mean_bias = np.mean(errors)
+        mean_bias = np.mean(differences)
         return mean_bias
 
+# Load dataset
+dataset = np.loadtxt("/Users/kas/Desktop/EDFA_ML-main/EDFA_1_18dB.csv", delimiter=",")
+X = dataset[:, 1:86]
+y = dataset[:, 86:]
+
+# Initialize model, optimizer, scheduler
+model = NN()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01, betas=(0.9, 0.999), eps=1e-08)
 scheduler = CosineAnnealingLR(optimizer, 20, 1e-5)
-loss_fn = nn.MSELoss()
 kf = KFold(n_splits=5, shuffle=True, random_state=0)
-ls = []
-params = model.state_dict()
-print("Weights of layer1:", params['layer1.0.weight'])
 prune_percentage = 50
+
+# Track best loss
+best_loss = float('inf')
+
+# Append validation loss to the list
+ls = []
+
+# Training loop
 for fold, (train_index, val_index) in enumerate(kf.split(X)):
     train_dataset = MyDataset(X[train_index], y[train_index])
     val_dataset = MyDataset(X[val_index], y[val_index])
     train_loader = DataLoader(train_dataset, batch_size=1024, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=1024, shuffle=False)
-
     # トータルパラメータ数
     total_parameters_before_pruning = sum(p.numel() for p in model.parameters())
     print("Before pruning - Total parameters:", total_parameters_before_pruning)
-
     for epoch in range(500):
-        train(model, train_loader, optimizer)
+        train(model, train_loader, optimizer, scheduler)
         val_loss = evaluate(model, val_loader)
-        print("Fold {}, Epoch {}, Val Loss: {:.4f}".format(fold, epoch + 1, val_loss))
+
+        # Print validation loss
+        print(f"Fold {fold}, Epoch {epoch + 1}, Val Loss: {val_loss:.4f}")
+        for param in model.parameters():
+            del param
         # すべての世代のガベージコレクションを実行
         gc.collect()
+        # Print memory usage
         get_memory_usage()
+        # Timer setup
+        timer_before_pruning = Timer(
+            stmt="model(data)",
+            setup="from __main__ import model, train_loader; data, _ = next(iter(train_loader))"
+        )
+        time_before_pruning = timer_before_pruning.timeit(100)
+        print(f"Inference time before pruning: {time_before_pruning} seconds")
+        # Prune at specified epochs
+        if epoch in [350, 400, 450]:
+            # Measure inference time before pruning
+            timer_before_pruning = Timer(
+                stmt="model(input_data)",
+                setup="from __main__ import model, input_data",
+            )
 
-        if fold == 0 and epoch == 0:
-            best_loss = val_loss
-        elif epoch % 50 == 0:  # 50エポックごとにプルーニング
+            # プルーニング前の各層のスパース性を確認
             for name, module in model.named_modules():
                 if isinstance(module, nn.Linear):
-                    # プルーニング前の各層のスパース性を確認
                     weights = module.weight.data
                     sparsity = float(torch.sum(weights == 0) / weights.numel())
                     print(f"Layer {name} Sparsity before pruning: {sparsity:.4f}")
                     print(f"Layer {name} Weights before pruning:\n{weights}")
 
-                    # プルーニング
-                    row_norms = torch.norm(weights, p=1, dim=1)
-                    threshold = torch.kthvalue(row_norms, int(row_norms.size(0) * (prune_percentage / 100.0)))[0]
-                    masks = (row_norms >= threshold).float().view(weights.size(0), 1)
-                    module.weight.data = weights * masks
+            for name, module in model.named_modules():
+                if isinstance(module, nn.Linear):
+                    weights = module.weight.data
+                    amount = prune_percentage / 100.0
+                    prune.l1_unstructured(module, name='weight', amount=amount)
+                    # プルーニング後のモデル
+                    model_after_pruning = NN()
+
 
                     # プルーニング後の各層のスパース性を確認
-                    sparsity = float(torch.sum(module.weight.data == 0) / module.weight.data.numel())
-                    print(f"Layer {name} Sparsity after pruning: {sparsity:.4f}")
-                    print(f"Layer {name} Weights after pruning:\n{module.weight.data}")
+                    for name, module in model_after_pruning.named_modules():
+                        if isinstance(module, nn.Linear):
+                            weights = module.weight.data
+                            print(f"Layer {name} Weights after pruning:\n{weights}")
+                            sparsity = float(torch.sum(weights == 0) / weights.numel())
+                            print(f"Layer {name} Sparsity after pruning: {sparsity:.4f}")
 
-            # トータルパラメータ数
-            total_parameters_after_pruning = sum(p.numel() for p in model.parameters())
-            print("Total parameters after pruning:", total_parameters_after_pruning)
+                    # Print total parameters after pruning
+                    print("Total parameters:", sum(p.numel() for p in model.parameters()))
 
-        elif val_loss < best_loss:
+                    # プルーニング前のモデルとプルーニング後のモデルのサイズ比較
+                    print(f"Model size before pruning: {sum(p.numel() for p in model.parameters())} parameters")
+                    print(
+                        f"Model size after pruning: {sum(p.numel() for p in model_after_pruning.parameters())} parameters")
+
+                    timer_after_pruning = Timer(
+                        stmt="model(data)",
+                        setup="from __main__ import model, train_loader; data, _ = next(iter(train_loader))"
+                    )
+                    time_after_pruning = timer_after_pruning.timeit(100)
+                    print(f"Inference time after pruning: {time_after_pruning} seconds")
+        # Save the best model
+        if val_loss < best_loss:
             best_loss = val_loss
-            filename = 'model_best_{}-{}.pth'.format(fold, epoch + 1)
+            filename = f'model_best_{fold}_{epoch + 1}.pth'
             torch.save(model.state_dict(), filename)
-            print("********************************BEST {}-RMSE: {:.2e}*****************************".format(fold,
-                                                                                                             best_loss))
-
+            print(f"********************************BEST {fold}-RMSE: {best_loss:.2e}*****************************")
         if epoch == 499:
             ls.append(best_loss)
 
@@ -160,8 +192,5 @@ p = pd.DataFrame(ls)
 filename = 'loss_pruned.csv'
 p.to_csv(filename)
 
-end_time = time.time()
-elapsed_time = end_time - start_time
-print(f"処理にかかった時間: {elapsed_time}秒")
 get_memory_usage()
 
